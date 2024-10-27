8dc138fa9444ca4bd07c39b7d85aebae23455c54
Michael Mwai <michaeldmwai@gmail.com>
2024-10-27 12:31:17 +0300

    Set up grid)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 import "./style.css";
 
+const sketchBoard = document.querySelector(".etch-area");
+
+function makeGrid(gridArea) {
+  for (let i = 0; i < gridArea; i++) {
+    const gridBox = document.createElement("div");
+    gridBox.classList.add("grid-box");
+    sketchBoard.appendChild(gridBox);
+  }
+}
+
+makeGrid(256);
+
 console.log("connected");
