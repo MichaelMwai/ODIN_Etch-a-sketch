803f8d640f3c511627fa35a9dea6f60f7addc7ae
Michael Mwai <michaeldmwai@gmail.com>
2024-10-27 14:32:12 +0300

    Refractor grid creation and hover effect into separate functions

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 // import "./style.css";
 
-// import "./style.css";
-
 const sketchBoard = document.querySelector(".etch-area");
 
+// Function to create the grid elements only
 function makeGrid(gridArea) {
   for (let i = 0; i < gridArea; i++) {
     const gridBox = document.createElement("div");
     gridBox.classList.add("grid-box");
     sketchBoard.appendChild(gridBox);
 
-    gridBox.addEventListener("mouseover", () => {
-      gridBox.style.backgroundColor = "black";
-    });
+    // Apply hover effect
+    applyHoverEffect(gridBox);
   }
+}
+
+// Function to apply hover effect on each grid box
+function applyHoverEffect(element) {
+  element.addEventListener("mouseover", () => {
+    element.style.backgroundColor = "black";
+  });
 }
 
 makeGrid(256);
